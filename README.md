46ce2c5ed02fcfc3891c1ecc6223526ab64f4ddb
Bryan Andrews <bryanandrews@gmail.com>
2018-05-19 17:00:55 -0700

    added pushover support and examples

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ play_random.sh
 -------------
 
 A Linux script to randomly play a number of MP3 files from a directory.
+
+Switches
+========
+1) `-d` Enable debugging.
+2) `-c` Set number of files to play.
+3) `-p` Set path to MP3 directory.
+4) `-h` Help/Usage.
+
+Using [optional] PushOver Notification Example
+==============================================
+~~~
+PUSHOVER_APPTOKEN=jdhfkasuhgfksadfhjk PUSHOVER_USERKEY=97325978f5394f ./play_random.sh -d
+~~~
+
+Setting a random Cronjob
+========================
+
+Start a job at 10:00am and 1:00pm Monday through Friday, then sleep for up to 3 hours before starting the job.
+
+~~~
+0 10,13 * * 1-5 sleep $(( $$ \% 10800 )); /home/pi/sfx/play_random.sh
+~~~
--- play_random.sh
+++ play_random.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #
-#  play a random list of MP3's to annoy the stupid ass dog.
+#  play a random list of MP3's
 #
-#  v0.0.1 - stop fucking barking at nothing rc1
+#  v0.1.0 - [optional] env variable PUSHOVER_APPTOKEN and PUSHOVER_USERKEY for remote notify
+#  v0.0.1 - initial release
 #
-#######################################################################
+######################################################################################################
 
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-sfxDir="/home/pi/sfx" # default dir with mp3 files
-playCount="3"         # default number of files to play
+ver="0.1.0"                                               # current script version
+sfxDir="/home/pi/sfx"                                     # default dir with mp3 files
+playCount="3"                                             # default number of files to play
+pushoverAPIURL="https://api.pushover.net/1/messages.json" # pushOver API URL
 
 while getopts ":dp:c:" opt; do
   case ${opt} in
@@ -23,7 +26,7 @@ while getopts ":dp:c:" opt; do
       playCount=$OPTARG
       ;;
     \? )
-      echo "Usage: $0 [-p ${sfxDir}] [-c ${playCount}] [-d]"
+      echo "${ver} Usage: $0 [-p ${sfxDir}] [-c ${playCount}] [-d]"
       echo
       echo "    -p   Path to dir with MP3s."
       echo "    -c   number of randomly picked files to play."
@@ -39,11 +42,14 @@ done
 #
 
 if [ $DEBUG ];then
-  echo "DEBUG: sfxDir=$sfxDir"
-  echo "DEBUG: playCount=$playCount"
+  echo "DEBUG: sfxDir=${sfxDir}"
+  echo "DEBUG: playCount=${playCount}"
+  echo "DEBUG: PUSHOVER_APPTOKEN=${PUSHOVER_APPTOKEN}"
+  echo "DEBUG: PUSHOVER_USERKEY=${PUSHOVER_USERKEY}"
 fi
 
-reqs=( logger find mpg123 sort head )
+# required 3rd party tools, if they don't exist in the path exit 2
+reqs=( logger find mpg123 sort head curl )
 for i in "${reqs[@]}"; do
   command -v $i >/dev/null 2>&1 || { echo >&2 "I require ${i} but it's not installed.  Aborting."; exit 2; }
 done
@@ -58,7 +64,7 @@ fi
 # build a playlist
 #
 
-playList=$(find "${sfxDir}" -name "*.mp3" | sort --random-sort | head -n ${playCount}) # random playlist string
+playList=$(find "${sfxDir}" -maxdepth 1 -name "*.mp3" | sort --random-sort | head -n ${playCount}) # random playlist string
 if [ -z "$playList" ];then
   logger -s -t $(basename $0) "Error no MP3 files found"
   exit 4
@@ -82,12 +88,25 @@ echo "$playList" | while IFS= read -r line ; do
   # loop through each line in playlist, verify, play mp3
   #
   if [ -f "$line" ];then
+
     test $DEBUG && echo "DEBUG: Playing MP3 \"$line\"..."
     logger -t $(basename $0) "Playing MP3 \"$line\""
     mpg123 -q "$line"
+    # pushover remote notification support
+    if [ -n $PUSHOVER_APPTOKEN -a -n $PUSHOVER_USERKEY ];then
+      curl -s --form-string "token=${PUSHOVER_APPTOKEN}" --form-string "user=${PUSHOVER_USERKEY}" --form-string "message=$(hostname) played $(basename "${line}")" ${pushoverAPIURL} > /dev/null
+      if [ $? -eq 0 ];then
+        logger -t $(basename $0) "Sent pushover notification to app_token ${PUSHOVER_APPTOKEN}"
+      else
+        logger -s -t $(basename $0) "Failed to send pushover notification to app_token:${PUSHOVER_APPTOKEN} user_key:${PUSHOVER_USERKEY}"
+      fi
+    fi
+
   else
+
     logger -s -t $(basename $0) "Error MP3 \"$line\" is not a file..."
     # exit 1
+
   fi
 
 done
